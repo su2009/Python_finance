bff43c9b026d7133ffdd9cf6bfe73f2cd4a72d56
su2009 <yangsu2009@gmail.com>
2017-02-25 09:06:09 -0800

    Update p5_Get_SP500_list.py

--- p5_Get_SP500_list.py
+++ p5_Get_SP500_list.py
@@ -12,7 +12,7 @@ def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class':'wikitable sortable'})
-    # 'class'='wikitable sortable'  SyntaxError: keyword can't be an expression  why?
+    # soup.find('table','class'='wikitable sortable')  SyntaxError: keyword can't be an expression  why?
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
